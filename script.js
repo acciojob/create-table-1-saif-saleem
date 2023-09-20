89b86a2cc2ecf73c7d9e08e2c4880865d92cbaaf
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-20 23:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function insert_Row() {
-    //Write your code here
-  
+function insert_Row(event) {
+	c=3;
+	let st=document.getElementById("sampleTable");
+    let row = document.createElement('tr');
+	row.innerHTML=`<td>Row${c} cell1</td> <td>Row${c} cell2</td>`;
+	
+  st.appendChild(row);
+	c++;
   
 }
